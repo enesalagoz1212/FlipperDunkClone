fb562f647be2f9b0a897133256ca204eb901c71a
enesalagoz1212 <118561378+enesalagoz1212@users.noreply.github.com>
2023-09-12 16:54:04 +0300

    vibration

---
+++ Assets/_GameFolder/Scripts/Managers/VibrationManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlipperDunkClone.Managers
+{
+	public class VibrationManager : MonoBehaviour
+	{
+		[SerializeField] private RichtapEffectSource _vibration;
+
+		private void OnEnable()
+		{
+			GameManager.OnGameEnd += OnGameEnd;
+		}
+		private void OnDisable()
+		{
+			
+			GameManager.OnGameEnd -= OnGameEnd;
+		}
+		public void VibrationOnBasketScore()
+		{
+			_vibration.Play();
+		}
+
+		public void VibrationOnGameEnd()
+		{
+			_vibration.Play();
+		}
+
+		private void OnGameEnd(bool isSuccesful)
+		{
+			Debug.Log("Vibration worked");
+			VibrationOnGameEnd();
+		}
+	}
+}
+
